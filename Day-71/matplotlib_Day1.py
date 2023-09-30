cbd4d930af4649e9bd597f7231b12cba9ce70794
Rahat <risulislam089@gmail.com>
2023-09-30 22:27:11 +0600

    Day 71 Matplotlib Day 1 Complete

---
+++ Day-71/matplotlib_Day1.py
@@ -0,0 +1,20 @@
+# Here I am showing Matplotlib Day 1
+
+# Matplotlib Pyplot Demo
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([0, 6])
+y = np.array([0, 10])
+
+plt.plot(x, y)
+plt.show
+
+# Matplotlib Pyplot Marker
+
+x = np.array([0, 6, 1, 25, 3, 50])
+
+plt.plot(x, marker='o' ) 
+plt.show
+
